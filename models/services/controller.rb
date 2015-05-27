66eee7f23646ca58013c9a3d6e04c03e5dd5e475
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-05-27 15:53:27 -0300

    auv_control: use data_service_type to define the controller services
    This makes sure that the definition line and documentation will show
    up in syskit browse

--- models/services/controller.rb
+++ models/services/controller.rb
@@ -6,42 +6,114 @@ require 'rock/models/services/joints_control_loop'
 
 module RockAUV
     module Services
-        Controller = Control::Element.new_submodel do
+        # Base service for components that generate commands
+        data_service_type 'Controller', parent: Control::Element do
             provides Rock::Services::Controller
         end
 
-        WorldPosController = Control::WorldPos.new_submodel do
+        # Base type for controllers that generate positions in the world frame
+        #
+        # Such a controller for x and yaw axis would be created with
+        #
+        # ```
+        # RockAUV::Services::Controller.for { WorldPos(:x, :yaw) }
+        # ```
+        data_service_type 'WorldPosController', parent: Control::WorldPos do
             provides Controller
             output_port 'cmd_out_world_pos', '/base/LinearAngular6DCommand'
         end
-        WorldVelController = Control::WorldVel.new_submodel do
+
+        # Base type for controllers that generate velocities in the world frame
+        #
+        # Such a controller for x and yaw axis would be created with
+        #
+        # ```
+        # RockAUV::Services::Controller.for { WorldVel(:x, :yaw) }
+        # ```
+        data_service_type "WorldVelController", parent: Control::WorldVel do
             provides Controller
             output_port 'cmd_out_world_vel', '/base/LinearAngular6DCommand'
         end
-        AlignedPosController = Control::AlignedPos.new_submodel do
+
+        # Base type for controllers that generate positions in the aligned frame
+        #
+        # Such a controller for x and yaw axis would be created with
+        #
+        # ```
+        # RockAUV::Services::Controller.for { AlignedPos(:x, :yaw) }
+        # ```
+        data_service_type "AlignedPosController", parent: Control::AlignedPos do
             provides Controller
             output_port 'cmd_out_aligned_pos', '/base/LinearAngular6DCommand'
         end
-        AlignedVelController = Control::AlignedVel.new_submodel do
+
+        # Base type for controllers that generate velocities in the aligned frame
+        #
+        # Such a controller for x and yaw axis would be created with
+        #
+        # ```
+        # RockAUV::Services::Controller.for { AlignedVel(:x, :yaw) }
+        # ```
+        data_service_type "AlignedVelController", parent: Control::AlignedVel do
             provides Controller
             output_port 'cmd_out_aligned_vel', '/base/LinearAngular6DCommand'
         end
-        AlignedEffortController = Control::AlignedEffort.new_submodel do
+
+        # Base type for controllers that generate effort in the aligned frame
+        #
+        # Such a controller for x and yaw axis would be created with
+        #
+        # ```
+        # RockAUV::Services::Controller.for { AlignedEffort(:x, :yaw) }
+        # ```
+        data_service_type 'AlignedEffortController', parent: Control::AlignedEffort do
             provides Controller
             output_port 'cmd_out_aligned_effort', '/base/LinearAngular6DCommand'
         end
-        BodyPosController = Control::BodyPos.new_submodel do
+
+        # Base type for controllers that generate positions in the body frame.
+        #
+        # Such a controller for x and yaw axis would be created with
+        #
+        # ```
+        # RockAUV::Services::Controller.for { BodyPos(:x, :yaw) }
+        # ```
+        data_service_type "BodyPosController", parent: Control::BodyPos do
             provides Controller
             output_port 'cmd_out_body_pos', '/base/LinearAngular6DCommand'
         end
-        BodyVelController = Control::BodyVel.new_submodel do
+
+        # Base type for controllers that generate velocities in the body frame
+        #
+        # Such a controller for x and yaw axis would be created with
+        #
+        # ```
+        # RockAUV::Services::Controller.for { BodyVel(:x, :yaw) }
+        # ```
+        data_service_type "BodyVelController", parent: Control::BodyVel do
             provides Controller
             output_port 'cmd_out_body_vel', '/base/LinearAngular6DCommand'
         end
-        BodyEffortController = Control::BodyEffort.new_submodel do
+
+        # Base type for controllers that generate effort in the body frame
+        #
+        # Such a controller for x and yaw axis would be created with
+        #
+        # ```
+        # RockAUV::Services::Controller.for { BodyEffort(:x, :yaw) }
+        # ```
+        data_service_type "BodyEffortController", parent: Control::BodyEffort do
             provides Controller
             output_port 'cmd_out_body_effort', '/base/LinearAngular6DCommand'
         end
+
+        # Base type for controllers that generate effort in the aligned frame
+        #
+        # Such a controller for x and yaw axis would be created with
+        #
+        # ```
+        # RockAUV::Services::Controller.for { AlignedEffort(:x, :yaw) }
+        # ```
         data_service_type 'BodyThrustController' do
             output_port 'cmd_out_body_thrust', '/base/samples/Joints'
             provides Rock::Services::JointsOpenLoopController,
