397c95777cce716f374ebc323dff9b5e96e013e5
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-06-05 13:58:44 -0300

    fix naming of test_direct_z_control
    Did not get renamed from test_direct_depth_control

--- test/suite_compositions.rb
+++ test/suite_compositions.rb
@@ -1,9 +1,10 @@
 require "test/compositions/suite_control"
-
-require "test/compositions/test_direct_depth_control"
-
+require "test/compositions/test_direct_z_control"
 require "test/compositions/test_direct_yaw_control"
 require "test/compositions/test_constant_setpoint_generator"
 require "test/compositions/test_constant_world_pos_z_setpoint_generator"
 require "test/compositions/test_constant_world_pos_yaw_setpoint_generator"
+
+
+
 
