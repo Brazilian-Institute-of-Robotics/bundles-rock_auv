4b62a388d130ae64ae0f38c6a69c33b9ebd529a2
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-06-23 12:48:45 -0300

    sort the various controllers in a meaningful way

--- models/compositions/stable_pitch_roll.rb
+++ models/compositions/stable_pitch_roll.rb
@@ -23,14 +23,13 @@ module RockAUV
     end
 end
 
-require "models/compositions/stable_pitch_roll/constant_world_pos_yaw_control"
-require "models/compositions/stable_pitch_roll/constant_aligned_vel_yaw_control"
-require "models/compositions/stable_pitch_roll/constant_world_pos_z_control"
-require "models/compositions/stable_pitch_roll/constant_aligned_vel_z_control"
 require "models/compositions/stable_pitch_roll/constant_world_pos_xy_control"
+require "models/compositions/stable_pitch_roll/constant_world_pos_z_control"
+require "models/compositions/stable_pitch_roll/constant_world_pos_yaw_control"
+require "models/compositions/stable_pitch_roll/hover"
 
 require "models/compositions/stable_pitch_roll/constant_aligned_vel_x_control"
-
 require "models/compositions/stable_pitch_roll/constant_aligned_vel_y_control"
+require "models/compositions/stable_pitch_roll/constant_aligned_vel_z_control"
+require "models/compositions/stable_pitch_roll/constant_aligned_vel_yaw_control"
 
-require "models/compositions/stable_pitch_roll/hover"
