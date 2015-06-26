d7b817dec662cfd510fc4dd466f7fd5a30757006
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-06-26 14:11:35 -0300

    test: don't run the rock_gazebo tests in live mode
    The rock-gazebo components need to be started inside gazebo's process,
    which we don't support right now.

--- test/orogen/test_rock_gazebo.rb
+++ test/orogen/test_rock_gazebo.rb
@@ -3,6 +3,10 @@ using_task_library 'rock_gazebo'
 module OroGen
     module RockGazebo
         describe ThrusterTask do
+            # Gazebo tasks need gazebo to be accessed ... which means
+            # interactive. Run these tests only in non-live mode
+            run_simulated
+
             it { is_configurable }
         end
     end
